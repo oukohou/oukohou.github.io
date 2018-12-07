404b8441c9f508ff7ec43738f8b7ff1193a55dd1
oukohou <oukohou@outlook.com>
2018-12-07 18:09:52 +0800

    bug fix: change baidu analytic code to my own.
    Signed-off-by: oukohou <oukohou@outlook.com>

--- assets/js/tongji.baidu.js
+++ assets/js/tongji.baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?47d019804542b27d414d139c3d142022";
-  var s = document.getElementsByTagName("script")[0]; 
+  hm.src = "https://hm.baidu.com/hm.js?ec7470452f344e57c8085be5be58583f";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
